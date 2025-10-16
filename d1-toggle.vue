b5da91ae0a97e284498a58a2a607df488736ee45
es. <111954059+jupyex@users.noreply.github.com>
2025-10-16 15:49:19 +0800

    Create d1-toggle.vue

---
+++ d1-toggle.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {ref, reactive} from 'vue'
+  const isShown = ref(true)
+  const btnText = ref('Hide the paragraph')
+  function toggle(){
+    isShown.value = !isShown.value
+    btnText.value = isShown.value ? 'Hide the paragraph' : 'Show the paragraph'
+  }
+</script>
+<template>
+  <p v-if="isShown">Hello</p>
+  <button v-on:click="toggle">{{ btnText }}</button>
+</template>
+
+<style>
+
+</style>
