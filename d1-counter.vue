4a551282b2b75fc54dcd8d7352c6bedeb1a4d50c
es. <111954059+jupyex@users.noreply.github.com>
2025-10-16 15:38:42 +0800

    Create d1-counter.vue

---
+++ d1-counter.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import {ref} from 'vue'
+  const counter = ref(0)
+  function increment(){
+    counter.value++
+  }
+  function decrement(){
+    counter.value--
+  }
+</script>
+<template>
+  <h1 style="text-align:center">Counter App</h1>
+  <p style="text-align:center">{{ counter }}</p>
+  <div style="text-align:center">
+    <button class="btn" v-on:click="increment">+1</button>
+    <button class="btn" v-on:click="decrement">-1</button>
+  </div>
+  
+</template>
+
+<style>
+.btn{
+  margin: 10px;
+  padding: 4px 15px 4px 15px
+}
+</style>
