7f385107f58608f08c7be5b552ae96986d591fe2
es. <111954059+jupyex@users.noreply.github.com>
2025-10-16 15:40:47 +0800

    Create d1-greetings.vue

---
+++ d1-greetings.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import {ref, reactive} from 'vue'
+  const name = ref('Name')
+</script>
+<template>
+  <input v-model="name"></input>
+  <h1>Hello, {{ name }}</h1>
+</template>
+
+<style>
+
+</style>
